b12afe306bd6e99c097b4a99a4a42e5011dc7848
jgeorge <jgeorge@sv2217.zit.bam.de>
2021-11-25 19:36:28 +0100

    Add example

---
+++ examples/example_script_NaCl.py
@@ -0,0 +1,24 @@
+import os
+
+from lobsterpy.cohp.analyze import Analysis
+from lobsterpy.cohp.describe import Description
+
+directory = "NaCl"
+
+# Setup analysis dict
+analyse = Analysis(path_to_poscar=os.path.join(directory, "POSCAR"),
+                   path_to_icohplist=os.path.join(directory, "ICOHPLIST.lobster"),
+                   path_to_cohpcar=os.path.join(directory, "COHPCAR.lobster"),
+                   path_to_charge=os.path.join(directory, "CHARGE.lobster"))
+
+# Setup Desciption dict
+describe = Description(analysis_object=analyse)
+describe.write_description()
+
+# Automatic plots
+describe.plot_cohps(ylim=[-10, 2], xlim=[-4, 4])
+
+# different dicts that summarize the results
+print(analyse.condensed_bonding_analysis)
+print(analyse.final_dict_bonds)
+print(analyse.final_dict_cations)
