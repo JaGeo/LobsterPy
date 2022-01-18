b51d5183a48431007a16f972846db29c1151fc0e
jgeorge <jgeorge@sv2218.zit.bam.de>
2022-01-18 09:39:38 +0100

    Update

--- lobsterpy/cli.py
+++ lobsterpy/cli.py
@@ -5,13 +5,20 @@ from lobsterpy.cohp.analyze import Analysis
 from lobsterpy.cohp.describe import Description
 from pymatgen.electronic_structure.cohp import CompleteCohp
 from pymatgen.electronic_structure.plotter import CohpPlotter
+from pymatgen.electronic_structure.core import Orbital
 
 parser = argparse.ArgumentParser(description='Analyze Lobster runs.')
 
 parser.add_argument('--description', action="store_true", default=False, help='will perform bonding analysis study')
 parser.add_argument('--automaticplot', action="store_true", default=False,
                     help='plots most important interactions automatically')
-parser.add_argument('--plotcohp', dest="plotcohp", nargs='+', default=None, type=int, help='plots specific cohps')
+parser.add_argument('--plotcohp', dest="plotcohp", nargs='+', default=None, type=int, help='plots specific cohps, list them after --plotcohp')
+
+parser.add_argument('--summed', action="store_true", help='if True and --plotcohp is used as well, then a summed COHP is shown')
+
+# TODO: add orbitalwise cohps
+#parser.add_argument('--orbitalwise', action="store_true", help='if True and --orbitalwise is used, all orbitalwise cohps are plotted')
+
 
 parser.add_argument('--integratecohp', action="store_true", help='integrate plots of most important interactions')
 
@@ -28,7 +35,10 @@ parser.add_argument('--json', action="store_true",
                     help='will produce a lobsterpy.json with the most important informations')
 parser.add_argument('--filename', default="lobsterpy.json", type=str,
                     help='path to ICOHPLIST.lobster. Default is "ICOHPLIST.lobster"')
+
 parser.add_argument('--allbonds', action="store_true",default=False, help='will consider all bonds, not only cation-anion bonds (default) ')
+
+
 
 args = parser.parse_args()
 
@@ -60,9 +70,16 @@ def main():
         cp = CohpPlotter()
         # get a nicer plot label
 
-        for label in args.plotcohp:
-            cp.add_cohp(label, completecohp.get_cohp_by_label(label=str(label)))
-            # check which COHP you are plotting
+        if not args.summed:
+            #if not args.orbitalwise:
+                for label in args.plotcohp:
+                    cp.add_cohp(label, completecohp.get_cohp_by_label(label=str(label)))
+            # else:
+            #     for label in args.plotcohp:
+            #         for orbitals in completecohp.orb_res_cohp[str(label)].keys():
+            #             cp.add_cohp(str(label)+': '+orbitals, completecohp.get_orbital_resolved_cohp(label=str(label), orbitals=orbitals))
+        else:
+            cp.add_cohp(str(args.plotcohp), completecohp.get_summed_cohp_by_label_list(label_list=[str(label) for label in args.plotcohp]))
 
         x = cp.get_plot(integrated=args.integratecohp)
         x.ylim(args.ylim)
