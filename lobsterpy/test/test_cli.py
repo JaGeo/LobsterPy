0ea8ae7818fb2f69bd9dfba8d5f680413443ffc0
JaGeo <janine.george@bam.de>
2022-02-23 19:09:33 +0100

    fix flake8

--- lobsterpy/test/test_cli.py
+++ lobsterpy/test/test_cli.py
@@ -29,19 +29,19 @@ class TestDescribe(unittest.TestCase):
 
     def test_description_allbonds(self):
         test = get_parser().parse_args(["description", "--allbonds"])
-        plot = run(test)
+        run(test)
 
     def test_plot(self):
         test = get_parser().parse_args(["plot", "1", "2"])
-        plot = run(test)
+        run(test)
 
     def test_plot_cobis(self):
         test = get_parser().parse_args(["plot", "1", "--cobis"])
-        plot = run(test)
+        run(test)
 
     def test_plot_coops(self):
         test = get_parser().parse_args(["plot", "1", "--coops"])
-        plot = run(test)
+        run(test)
 
     def test_plot_summed(self):
         test = get_parser().parse_args(
@@ -52,58 +52,58 @@ class TestDescribe(unittest.TestCase):
                 "2",
             ]
         )
-        plot = run(test)
+        run(test)
 
     def test_plot_orbitalwise(self):
         test = get_parser().parse_args(
             ["plot", "1", "2", "--orbitalwise", "3s-3s", "3s-3s"]
         )
-        plot = run(test)
+        run(test)
 
     def test_plot_orbitalwise_all(self):
         test = get_parser().parse_args(["plot", "1", "--orbitalwise", "all"])
-        plot = run(test)
+        run(test)
 
     def test_plot_errors(self):
         with self.assertRaises(IndexError):
             test = get_parser().parse_args(["plot", "1", "2", "--orbitalwise", "3s-3s"])
-            plot = run(test)
+            run(test)
 
         with self.assertRaises(IndexError):
             test = get_parser().parse_args(["plot", "400000"])
-            plot = run(test)
+            run(test)
 
         with self.assertRaises(IndexError):
             test = get_parser().parse_args(["plot", "1", "--orbitalwise", "1s-1s"])
-            plot = run(test)
+            run(test)
 
     def test_plot_fontsize(self):
         test = get_parser().parse_args(["plot", "1", "--fontsize", "20"])
-        plot = run(test)
+        run(test)
 
     def test_plot_width_height(self):
         test = get_parser().parse_args(["plot", "1", "--width", "20", "--height", "20"])
-        plot = run(test)
+        run(test)
 
     def test_plot_width(self):
         test = get_parser().parse_args(["plot", "1", "--width", "20"])
-        plot = run(test)
+        run(test)
 
     def test_plot_height(self):
         test = get_parser().parse_args(["plot", "1", "--height", "20"])
-        plot = run(test)
+        run(test)
 
     def test_plot_save(self):
         with TemporaryDirectory() as d:
             temp_file_name = os.path.join(d, "test.pdf")
             test = get_parser().parse_args(["plot", "1", "--saveplot", temp_file_name])
-            plot = run(test)
+            run(test)
 
     def test_json(self):
         with TemporaryDirectory() as d:
             temp_file_name = os.path.join(d, "test.json")
             test = get_parser().parse_args(["automatic-plot", "--json"])
-            plot = run(test)
+            run(test)
 
     def tearDown(self) -> None:
         os.chdir(CurrentDir)
