48a3e9a50ce15c90524620fdf089fe48779e60c0
JaGeo <janine.george@bam.de>
2022-02-23 18:27:08 +0100

    increase coverage of tests

--- lobsterpy/test/test_cli.py
+++ lobsterpy/test/test_cli.py
@@ -1,12 +1,12 @@
-import unittest
 import os
+import unittest
 from pathlib import Path
 
-from lobsterpy.cohp.analyze import Analysis
 from lobsterpy.cli import get_parser, run
 
 CurrentDir = Path(__file__).absolute().parent
 TestDir = CurrentDir / "../"
+
 
 # TODO: check if text outputs and plots are really generated
 
@@ -30,6 +30,33 @@ class TestDescribe(unittest.TestCase):
     def test_description_allbonds(self):
         test = get_parser().parse_args(["description", "--allbonds"])
         plot = run(test)
+
+    def test_plot(self):
+        test = get_parser().parse_args(["plot", "1", "2"])
+        plot = run(test)
+
+    def test_plot_summed(self):
+        test = get_parser().parse_args(["plot", "--summed", "1", "2", ])
+        plot = run(test)
+
+    def test_plot_orbitalwise(self):
+        test = get_parser().parse_args(["plot", "1", "2", "--orbitalwise", "3s-3s", "3s-3s"])
+        plot = run(test)
+
+    def test_plot_errors(self):
+        with self.assertRaises(IndexError):
+            test = get_parser().parse_args(["plot", "1", "2", "--orbitalwise", "3s-3s"])
+            plot = run(test)
+
+        with self.assertRaises(IndexError):
+            test = get_parser().parse_args(["plot", "400000"])
+            plot = run(test)
+
+        with self.assertRaises(IndexError):
+            test = get_parser().parse_args(["plot", "1", "--orbitalwise", "1s-1s"])
+            plot = run(test)
+
+
 
     def tearDown(self) -> None:
         os.chdir(CurrentDir)
