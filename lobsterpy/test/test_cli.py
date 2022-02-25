4a6d4d28c166cc7b29bc9421f8f428d8723fd6ab
Adam J. Jackson <a.j.jackson@physics.org>
2022-02-25 16:24:33 +0000

    CLI tests: Refactor to pytest, JSON reference file
    Currently only stdout is checked, but this can be extended to plotting
    data as well.
    
    Mocking is used to suppress calls to plt.show(); I was finding that
    with xvfb, plot calls sometimes led to indefinite hanging.

--- lobsterpy/test/test_cli.py
+++ lobsterpy/test/test_cli.py
@@ -1,108 +1,92 @@
+from contextlib import redirect_stdout
+import io
+import json
 import os
-import unittest
 from pathlib import Path
-from tempfile import TemporaryDirectory
+
+import pytest
+
 from lobsterpy.cli import get_parser, run
 
 CurrentDir = Path(__file__).absolute().parent
 TestDir = CurrentDir / "../"
+ref_data_file = TestDir / "TestData/cli-reference.json"
+test_args = [
+    ["automatic-plot"],
+    ["automaticplot", "--allbonds"],
+    ["description"],
+    ["description", "--all-bonds"],
+    ["plot", "1", "2"],
+    ["plot", "1", "--cobis"],
+    ["plot", "1", "--coops"],
+    ["plot", "--summed", "1", "2"],
+    ["plot", "1", "2", "--orbitalwise", "3s-3s", "3s-3s"],
+    ["plot", "1", "--orbitalwise", "all"],
+    ["plot", "1", "--fontsize", "20"],
+    ["plot", "1", "--width", "20", "--height", "20"],
+    ["plot", "1", "--width", "20"],
+    ["plot", "1", "--height", "20"],
+]
+
+error_test_cases = [
+    (["plot", "1", "2", "--orbitalwise", "3s-3s"], IndexError),
+    (["plot", "400000"], IndexError),
+    (["plot", "1", "--orbitalwise", "1s-1s"], IndexError),
+]
 
 
-# TODO: check if text outputs and plots are really generated
+# TODO: check if plots are really generated
+class TestCLI:
+    @pytest.fixture
+    def inject_mocks(self, mocker):
+        # Prevent calls to show so we can get the current figure using gcf()
+        mocker.patch("matplotlib.pyplot.show")
+        mocker.resetall()
 
+    with open(ref_data_file, "r") as fd:
+        ref_results = json.load(fd)
 
-class TestDescribe(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setup_class(cls):
         os.chdir(TestDir / "TestData/NaCl")
 
-    def test_automaticplot(self):
-        test = get_parser().parse_args(["automatic-plot"])
+    @pytest.mark.parametrize("args", test_args)
+    def test_cli_results(self, args, capsys, inject_mocks):
+        test = get_parser().parse_args(args)
         run(test)
 
-    def test_automaticplot_allbonds(self):
-        test = get_parser().parse_args(["automaticplot", "--allbonds"])
-        run(test)
+        captured = capsys.readouterr()
+        assert captured.out == self.ref_results[" ".join(args)]["stdout"]
 
-    def test_description(self):
-        test = get_parser().parse_args(["description"])
-        run(test)
-
-    def test_description_allbonds(self):
-        test = get_parser().parse_args(["description", "--allbonds"])
-        run(test)
-
-    def test_plot(self):
-        test = get_parser().parse_args(["plot", "1", "2"])
-        run(test)
-
-    def test_plot_cobis(self):
-        test = get_parser().parse_args(["plot", "1", "--cobis"])
-        run(test)
-
-    def test_plot_coops(self):
-        test = get_parser().parse_args(["plot", "1", "--coops"])
-        run(test)
-
-    def test_plot_summed(self):
-        test = get_parser().parse_args(
-            [
-                "plot",
-                "--summed",
-                "1",
-                "2",
-            ]
-        )
-        run(test)
-
-    def test_plot_orbitalwise(self):
-        test = get_parser().parse_args(
-            ["plot", "1", "2", "--orbitalwise", "3s-3s", "3s-3s"]
-        )
-        run(test)
-
-    def test_plot_orbitalwise_all(self):
-        test = get_parser().parse_args(["plot", "1", "--orbitalwise", "all"])
-        run(test)
-
-    def test_plot_errors(self):
-        with self.assertRaises(IndexError):
-            test = get_parser().parse_args(["plot", "1", "2", "--orbitalwise", "3s-3s"])
+    @pytest.mark.parametrize("args, error", error_test_cases)
+    def test_cli_errors(self, args, error, inject_mocks):
+        with pytest.raises(error):
+            test = get_parser().parse_args(args)
             run(test)
 
-        with self.assertRaises(IndexError):
-            test = get_parser().parse_args(["plot", "400000"])
-            run(test)
+    @pytest.mark.skip(reason="Only enable this test to regenerate test data")
+    def test_generate_ref_data(self, inject_mocks):
+        json_data = {}
 
-        with self.assertRaises(IndexError):
-            test = get_parser().parse_args(["plot", "1", "--orbitalwise", "1s-1s"])
-            run(test)
+        for args in test_args:
+            with redirect_stdout(io.StringIO()) as stdout:
+                test = get_parser().parse_args(args)
+                run(test)
+                json_data[" ".join(args)] = {"stdout": stdout.getvalue()}
 
-    def test_plot_fontsize(self):
-        test = get_parser().parse_args(["plot", "1", "--fontsize", "20"])
-        run(test)
+        with open(ref_data_file, "w") as fd:
+            json.dump(json_data, fd, indent=4, sort_keys=True)
 
-    def test_plot_width_height(self):
-        test = get_parser().parse_args(["plot", "1", "--width", "20", "--height", "20"])
-        run(test)
+    # def test_plot_save(self):
+    #     with TemporaryDirectory() as d:
+    #         temp_file_name = os.path.join(d, "test.pdf")
+    #         test = get_parser().parse_args(["plot", "1", "--saveplot", temp_file_name])
+    #         run(test)
 
-    def test_plot_width(self):
-        test = get_parser().parse_args(["plot", "1", "--width", "20"])
-        run(test)
+    # def test_json(self):
+    #     with TemporaryDirectory():
+    #         test = get_parser().parse_args(["automatic-plot", "--json"])
+    #         run(test)
 
-    def test_plot_height(self):
-        test = get_parser().parse_args(["plot", "1", "--height", "20"])
-        run(test)
-
-    def test_plot_save(self):
-        with TemporaryDirectory() as d:
-            temp_file_name = os.path.join(d, "test.pdf")
-            test = get_parser().parse_args(["plot", "1", "--saveplot", temp_file_name])
-            run(test)
-
-    def test_json(self):
-        with TemporaryDirectory():
-            test = get_parser().parse_args(["automatic-plot", "--json"])
-            run(test)
-
-    def tearDown(self) -> None:
+    def teardown_class(self) -> None:
         os.chdir(CurrentDir)
