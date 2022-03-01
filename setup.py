e4edef77722aa40fb1c51c1e15ee707ca8a5723b
JaGeo <janine.george@bam.de>
2022-03-01 11:38:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup,find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text(),
+long_description = (this_directory / "README.md").read_text('utf-8'),
 
 setup(
     name='lobsterpy',
     version='0.1.0',
     description='Package for autmatic bonding analysis with Lobster/VASP',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/jageo/lobsterpy',
     author='Janine George',
     author_email='janine.george@bam.de',
