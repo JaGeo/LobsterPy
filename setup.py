69a9bcb85bef361f521a1015f44b6a807ae4ab4c
JaGeo <janine.george@bam.de>
2022-03-01 12:16:05 +0100

    Update setuppy

--- setup.py
+++ setup.py
@@ -1,40 +1,41 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text('utf-8')[1:],
+long_description = (this_directory / "README.md").read_text("utf-8")
 setup(
-    name='lobsterpy',
-    version='0.1.0',
-    description='Package for autmatic bonding analysis with Lobster/VASP',
+    name="lobsterpy",
+    version="0.1.0",
+    description="Package for autmatic bonding analysis with Lobster/VASP",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/jageo/lobsterpy',
-    author='Janine George',
-    author_email='janine.george@bam.de',
-    license='BSD 3-clause',
+    long_description_content_type="text/markdown",
+    url="https://github.com/jageo/lobsterpy",
+    author="Janine George",
+    author_email="janine.george@bam.de",
+    license="BSD 3-clause",
     packages=find_packages(),
-    install_requires=['pymatgen>=2022.1.24',
-                      'numpy',
-                      'typing',
-                      ],
-
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+    install_requires=[
+        "pymatgen>=2022.1.24",
+        "numpy",
+        "typing",
     ],
-
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
     entry_points={
         "console_scripts": [
             "lobsterpy = lobsterpy.cli:main",
-
         ]
     },
-    package_data={'lobsterpy': ['plotting/lobsterpy_base.mplstyle',]}
-
+    package_data={
+        "lobsterpy": [
+            "plotting/lobsterpy_base.mplstyle",
+        ]
+    },
 )
