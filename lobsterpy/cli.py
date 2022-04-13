bd604efc28cdfd27484755b1b94678f4f33fcafc
JaGeo <janine.george@bam.de>
2022-04-13 21:24:51 +0200

    Fix docs

--- lobsterpy/cli.py
+++ lobsterpy/cli.py
@@ -102,7 +102,8 @@ def get_parser() -> argparse.ArgumentParser:
     plotting_parent = argparse.ArgumentParser(add_help=False)
     plotting_group = plotting_parent.add_argument_group("Plotting")
 
-    broadening_group = plotting_group.add_mutually_exclusive_group("Broadening")
+    broadening_group = plotting_group.add_mutually_exclusive_group()
+    broadening_group.add_argument_group("Broadening")
     broadening_group.add_argument(
         "--sigma",
         type=float,
