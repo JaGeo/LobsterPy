c1ed49dacf33572f3f76d6f9dcade62cd218afd8
JaGeo <janine.george@bam.de>
2022-04-13 21:06:21 +0200

    Name broadening group cli

--- lobsterpy/cli.py
+++ lobsterpy/cli.py
@@ -102,7 +102,7 @@ def get_parser() -> argparse.ArgumentParser:
     plotting_parent = argparse.ArgumentParser(add_help=False)
     plotting_group = plotting_parent.add_argument_group("Plotting")
 
-    broadening_group = plotting_group.add_mutually_exclusive_group()
+    broadening_group = plotting_group.add_mutually_exclusive_group("Broadening")
     broadening_group.add_argument(
         "--sigma",
         type=float,
