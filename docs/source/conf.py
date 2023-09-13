d3c193f9c74bdc13db53b2699cf7f975b5d0a1a1
Aakash Ashok Naik <91958822+naik-aakash@users.noreply.github.com>
2023-09-13 09:13:57 +0200

    Update docs/source/conf.py
    Co-authored-by: J. George <JaGeo@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 
-project = "Lobsterpy"
+project = "LobsterPy"
 copyright = "2022-2023, LobsterPy Development Team"
 author = "Janine George"
 
