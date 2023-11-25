ecff5a8cbd6e7af0daf35f926964934144813329
anaik <anaik@sv2218.zit.bam.de>
2023-11-25 07:52:35 +0100

    fix doc-string inconsistensies of structuregraph.graph  module

--- lobsterpy/structuregraph/graph.py
+++ lobsterpy/structuregraph/graph.py
@@ -68,6 +68,7 @@ class LobsterGraph:
         :param path_to_icooplist: path to ICOOPLIST.lobster (e.g., "ICOOPLIST.lobster").
         :param path_to_icobilist: path to ICOBILIST.lobster (e.g., "ICOBILIST.lobster").
         :param path_to_madelung: path to MadelungEnergies.lobster (e.g., "MadelungEnergies.lobster")
+        :param cutoff_icohp: only bonds that are stronger than "cutoff icohp * strongest ICOHP" will be considered.
         :param add_additional_data_sg: if True will add the information from ICOOPLIST.lobster
             and ICOBILIST.lobster based on ICOHPLIST.lobster relevant bond.
         :param which_bonds: selects which kind of bonds are analyzed. "all" is the default.
