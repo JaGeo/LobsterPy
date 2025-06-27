9eb55f8026d36e859eaba2c4b37a44f5094e9202
naik-aakash <aakash.naik@bam.de>
2025-06-27 08:19:07 +0200

    adapt tests to reflect new changes

--- tests/cohp/test_describe.py
+++ tests/cohp/test_describe.py
@@ -369,12 +369,12 @@ class TestCalcQualityDescribeWarnings:
 
         expected_warnings = [
             "Consider using DOSCAR.LSO.lobster, as non LSO DOS from LOBSTER can have negative DOS values",
-            "The specified number of bins for DOS comparisons (500) exceeds the number of available data points. "
-            "The number of bins will be set to the minimum available data points: 301.",
             "Minimum energy range requested for DOS comparisons is not available in VASP or LOBSTER calculation. "
-            "Thus, setting min_e to -5 eV",
+            "Thus, setting `min_e` to the minimum possible value of -15 eV",
             "Maximum energy range requested for DOS comparisons is not available in VASP or LOBSTER calculation. "
-            "Thus, setting max_e to 0 eV",
+            "Thus, setting `max_e` to the maximum possible value of 5 eV",
+            "Number of bins requested for DOS comparisons is larger than the number of points in the energy interval. "
+            "Thus, setting `n_bins` to 107.",
             "Input DOS files have very few points in the energy interval and thus comparisons will not be reliable. "
             "Please rerun the calculations with higher number of DOS points. "
             "Set NEDOS and COHPSteps tags to >= 2000 in VASP and LOBSTER calculations, respectively.",
@@ -390,8 +390,8 @@ class TestCalcQualityDescribeWarnings:
             "The absolute and total charge spilling for the calculation is 2.255 and 12.72 %, respectively.",
             "The projected wave function is completely orthonormalized as no bandOverlaps.lobster file is "
             "generated during the LOBSTER run.",
-            "The Tanimoto index from DOS comparisons in the energy range between -5, 0 eV for s, p, summed orbitals "
-            "are: 0.111, 0.0714, 0.0686.",
+            "The Tanimoto index from DOS comparisons in the energy range between -15, 5 eV for s, p, summed orbitals "
+            "are: 0.6712, 0.8113, 0.8064.",
         ]
 
         with warnings.catch_warnings(record=True) as w2:
